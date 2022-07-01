d641e3188a880d4a74859be2d2cab98f81a428aa
Mutegeki Ronald <murogive@gmail.com>
2022-07-01 06:22:51 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
