ec0e6c181e3b5b30205b6c4c7d94e474d97fa9f3
Mutegeki Ronald <murogive@gmail.com>
2022-07-02 11:29:14 +0900

    Added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,37 +8,35 @@
 
 int main(void)
 {
-	int a, b, c, d, option1, option2;
+int a, b, c, d, option1, option2;
 
-	for (a = 48; a < 58; a++)
+for (a = 48; a < 58; a++)
+{
+	for (b = 48; b < 58; b++)
 	{
-		for (b = 48; b < 58; b++)
+		for (c = 48; c < 58; c++)
 		{
-			for (c = 48; c < 58; c++)
+			for (d = 48; d < 58; d++)
 			{
-				for (d = 48; d < 58; d++)
+				option1 = (a * 10) + b;
+				option2 = (c * 10) + d;
+				if (option1 < option2)
 				{
-					option1 = (a * 10) + b;
-					option2 = (c * 10) + d;
-					if (option1 < option2)
-					{
-						putchar (a);
-						putchar (b);
-						putchar (' ');
-						putchar (c);
-						putchar (d);
-						if (a == 57 && b == 56 && c == 57 && d == 57)
-						{
-							break;
-						}
-						putchar (',');
-						putchar (' ');
-					}
+					putchar (a);
+					putchar (b);
+					putchar (' ');
+					putchar (c);
+					putchar (d);
+					if (a == 57 && b == 56 && c == 57 && d == 57)
+						break;
+					putchar (',');
+					putchar (' ');
 				}
 			}
 		}
 	}
+}
 
-	putchar ('\n');
-	return (0);
+putchar ('\n');
+return (0);
 }
