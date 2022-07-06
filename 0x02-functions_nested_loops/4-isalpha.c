2b4eec38a79c5710624a541f1306e939cddaadb3
Mutegeki Ronald <murogive@gmail.com>
2022-07-05 19:22:22 -0700

    Added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether a character is alphanumeric
+ * @c - an int representing the ASCII code for the character
+ *
+ * Return: 1 if c is a letter and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 91)
+	{ 
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
