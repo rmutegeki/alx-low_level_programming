71d256ac2404fd01d432150a550e51430cd41493
Mutegeki Ronald <murogive@gmail.com>
2022-07-05 18:09:18 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * main - prints _putchar and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[] = "_putchar";
+	int size = sizeof(text) / sizeof(char);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
