61dc78251bbfcaace8762f5c734395f0081f82c6
Mutegeki Ronald <murogive@gmail.com>
2022-07-18 07:46:21 -0700

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: a string of characters
+ * @c: the character to be located
+ *
+ * Return: the location of the charater c or NULL when c not in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if (*(s + count) == c)
+			return (*(s + count));
+		count++;
+	}
+
+	return (NULL);
+}
