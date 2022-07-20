007c67b321b4862e6f25c2a89bf4968ac8934bab
Mutegeki Ronald <murogive@gmail.com>
2022-07-20 07:52:14 -0700

    Added 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raising x^y
+ * @x: the base -> number to raise
+ * @y: the power
+ *
+ * Return: x raised to the power of y. -1 when y < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_resursion(x, y - 1));
+}
